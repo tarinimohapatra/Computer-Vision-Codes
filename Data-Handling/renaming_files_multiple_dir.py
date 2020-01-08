71ee3f246ee019ef6a4b6c2e8d1972f34285e172
Tarini Tanaya Mohapatra <doyouevercheckmail@gmail.com>
2020-01-08 12:50:35 +0530

    adding first commit in local repo

---
+++ Data-Handling/renaming_files_multiple_dir.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 10:36:48 2019
+
+@author: tarinitanayamohapatra
+"""
+
+import os
+
+path = os.getcwd()
+
+# path to the data folder
+data_path = ''
+#data_dir = 'daisy'
+data_dir_list = os.listdir(os.path.join(data_path))
+
+for data_dir in data_dir_list:
+    print ('Renaming files from folder: {}'.format(data_dir))
+    data_list = os.listdir(os.path.join(data_path,data_dir))
+    os.chdir(os.path.join(data_path,data_dir))
+
+    # The base name of image files
+    base_name=data_dir
+    
+    for i in range(len(data_list)):
+        img_name = data_list[i]
+#        img_rename = base_name + '_{}'.format(i+1)+'.png' # here the file name is base_name_1.png
+        img_rename = base_name + '_{:06d}'.format(i+1)+'.png' # here the file name is base_name_000001.png
+        if not os.path.exists(img_rename):
+            os.rename(img_name,img_rename)
+        
+    print ('------Complete renaming all folders: {}------'.format(data_dir))
+
+    os.chdir(path)
+
+print ('----Complete renaming files from all folders----')
